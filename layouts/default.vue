7e7ca5d88c4711f1ff18877eccf960246aca53c6
Dixy-05 <ddbarahona89@gmail.com>
2022-01-03 22:59:17 -0600

    new assets/site.scss

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  div
+    nav.navbar.header.has-shadow.is-base(role='navigation' aria-label='main navigation')
+      .navbar-brand
+        a.navbar-item(href='/')
+          h2 assessment
+        .navbar-burger
+          span
+            span
+              span
+    .container
+      nuxt
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+  div
+    h1 hello
+
+</template>
+
+<script>
+// import Card from '~/components/Card'
+
+export default {
+  name: 'IndexPage',
+  // components: {
+  //   Card,
+  // },
+}
+</script>
